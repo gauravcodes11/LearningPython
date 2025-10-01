6f47856ff09c7f5c386f22a934e77bd2c920bfff
gauravcodes11 <gauravupreti955@gmail.com>
2025-10-01 23:17:14 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+#string
+'''s=" gaurav upreti"
+print(s)
+print(type(s))
+print(s[1])
+print(s[0])
+print(s[-1])
+print(s[-3])'''
+
+#list
+print(a)
+
+l=("gaurav","chess","cricket","gaurav")
+print(min(l))
+
+#tuple
+tup=('gaurav', 'raghav')
+print(tup)
+
+tup1='gaurav','raghav'  #tuple packing
+print(tup)
+
+tup2=()
+print(tup2)
+
+tup3=('gaurav',)
+print(tup3)
+print(tup3[0])
