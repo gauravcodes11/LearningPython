fe2823cede78e88161690a5b3b78c81a7d2fbf70
Gaurav Upreti <gauravupreti955@gmail.com>
2026-04-04 08:52:12 +0530

    added some list comprehension examples

--- datatypes4.py
+++ datatypes4.py
@@ -23,4 +23,15 @@ for i in range(0,5):
     print(b[i])
 #extend() method
 b.extend([6,7,8,9,10])
-print(b)
+print(b) 
+
+#ListComprehension
+a=[1,2,3,4,5]
+res=[val**2 for val in a ]
+print(res)
+    #example 2-creating a list from a range
+a=[i for i in range(9)]
+print(a)
+    #example 3-nested loops
+a=[(x,y) for x in range(3) for y in range (3)] 
+print(a)
